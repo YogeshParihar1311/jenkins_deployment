fe91b2836458c797f26fb320f96489667cbdcfee
Yogesh Parihar <yogeshparihar818@gmail.com>
2023-07-06 20:36:18 +0530

    view changed

--- app/views.py
+++ app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def test(r):
-    return HttpResponse("<h1>Jenkins Deployment</h1>")
+    return HttpResponse("<h1>Jenkins Deployment CI/CD</h1>")
